3ab38fb14a9a4ef7e887af955e85dddd673fa612
John Cleaver <cleaver.john.k@gmail.com>
2019-04-20 21:28:56 -0400

    Add MDI

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
