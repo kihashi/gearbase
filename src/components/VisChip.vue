cf9e922e936c5b6f712058a9434485e017e58953
John Cleaver <cleaver.john.k@gmail.com>
2019-05-18 11:35:25 -0400

    Add a chip that shows visiblity

---
+++ src/components/VisChip.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-chip>
+        {{ visibility }}
+        <v-icon right>{{ icon }}</v-icon>
+    </v-chip> 
+</template>
+
+<script>
+export default {
+    name: 'VisChip',
+    props: {
+        visibility: {
+            default: () => {
+                return 'Public'
+            }
+        }
+    },
+    computed: {
+        icon: function () {
+            switch(this.visibility){
+                case 'Public':
+                    return 'mdi-earth'
+                case 'Unlisted':
+                    return 'mdi-eye-off'
+                case 'Private':
+                    return 'mdi-account'
+                default:
+                    return 'mdi-earth'
+            }
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
