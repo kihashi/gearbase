d64134a40e6507d5614adde33a03d5664cc90950
John Cleaver <cleaver.john.k@gmail.com>
2019-05-18 11:41:02 -0400

    Add login components to menu

--- src/components/UserMenu.vue
+++ src/components/UserMenu.vue
@@ -1,8 +1,18 @@
 <template>
     <span>
         <span v-if="!isAuthenticated">
-            <v-btn to="/signup" flat>Sign Up</v-btn>
-            <v-btn to="/login" color="red">Login</v-btn>
+            <v-dialog v-model="signUp" max-width=500>
+                <template v-slot:activator="{ on }">
+                    <v-btn v-on="on" flat>Sign Up</v-btn>
+                </template>
+                <Signup></Signup>
+            </v-dialog>
+            <v-dialog v-model="login" max-width=500>
+                <template v-slot:activator="{ on }">
+                    <v-btn v-on="on" color="red">Login</v-btn>
+                </template>
+                <Login></Login>
+            </v-dialog>
         </span>
         <span v-else>
             <v-btn @click="logout">
@@ -13,9 +23,17 @@
 </template>
 
 <script>
+import Signup from '@/views/Signup.vue'
+import Login from '@/views/Login.vue'
 export default {
     name: 'UserMenu',
+    components: {
+        Signup,
+        Login
+    },
     data: () => ({
+        signUp: false,
+        login: false
     }),
     computed: {
         isAuthenticated() {
